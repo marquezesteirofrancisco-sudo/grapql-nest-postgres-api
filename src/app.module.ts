27ba0242e1a266e9464efc6828c51211f74cfbf9
Francisco Marquez Esteiro <117361707+franciscomes@users.noreply.github.com>
2026-04-07 18:19:08 +0200

    commit inicial
    commit inicial

---
+++ src/app.module.ts
@@ -0,0 +1,40 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { ItemsModule } from './items/items.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { validateHeaderName } from 'http';
+
+
+@Module({
+  imports: [
+        ConfigModule.forRoot({isGlobal: true,}),
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+        driver: ApolloDriver,
+        // debug: false,
+        playground: false,
+        autoSchemaFile: join( process.cwd(), 'src/schema.gql'),
+        plugins: [
+          ApolloServerPluginLandingPageLocalDefault(), // Activamos la nueva interfaz
+        ]}),
+
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: process.env.DB_HOST,
+          port: +(process.env.DB_PORT ?? 5432),
+          username: process.env.DB_USERNAME,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_NAME,
+          synchronize: true,
+          autoLoadEntities: true,
+        }),
+
+        ItemsModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
