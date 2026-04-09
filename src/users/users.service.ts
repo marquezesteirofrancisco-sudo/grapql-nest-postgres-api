cd72203058dbe35d5725a47c63fab6f0c1543b3d
Francisco Marquez Esteiro <117361707+franciscomes@users.noreply.github.com>
2026-04-09 17:31:49 +0200

    Manejo de errores en signup
    Manejo de errores en signup

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,14 +1,17 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
 import { User } from './entities/user.entity';
 import { SingUpInput } from 'src/auth/dto/inputs/singup.input';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Int } from '@nestjs/graphql';
 
 @Injectable()
 export class UsersService {
 
+  private readonly logger: Logger = new Logger('UsersService');
+  
   constructor(
     @InjectRepository(User)
     private readonly usersRepository: Repository<User> /* TODO: Inyectar el repositorio de usuarios */
@@ -30,8 +33,7 @@ export class UsersService {
     }
     catch(error)
     {
-      console.log({error});
-      throw new BadRequestException('Error al crear el usuario');
+      this.handleDBExceptions(error);
     }
      
   }
@@ -50,5 +52,20 @@ export class UsersService {
 
   block(id: string) : Promise<User> {
     throw new Error('Method not implemented.');
+  }
+
+  private handleDBExceptions(error: any): never {
+
+    // error.code 23505 es un error de violación de restricción de unicidad en PostgreSQL
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail);
+    }
+    
+    // TODO: manejar otros errores de la base de datos
+    this.logger.error(error);
+
+    // lanzar una excepción genérica para el cliente
+    throw new InternalServerErrorException('Unexpected error, check server logs');
+
   }
 }
