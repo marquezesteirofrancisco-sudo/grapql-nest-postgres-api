64919ec47c707ebc0bd683dfb6323dbb2fcb7ca2
Francisco Marquez Esteiro <117361707+franciscomes@users.noreply.github.com>
2026-04-15 19:04:42 +0200

    GraphQL Enumerations
    GraphQL Enumerations

--- src/auth/enums/valid-roles.enum.ts
+++ src/auth/enums/valid-roles.enum.ts
@@ -1,3 +1,6 @@
+import { registerEnumType } from "@nestjs/graphql";
+import { register } from "module";
+
 //TODO: implementar enum como un GraphQL Enum Type
 export enum ValidRoles{
 
@@ -5,3 +8,8 @@ export enum ValidRoles{
     user = 'user',
     superUser = 'superuser',
 }
+
+registerEnumType(ValidRoles, {
+    name: 'ValidRoles',
+    description: 'Roles de usuario validos...'
+})
