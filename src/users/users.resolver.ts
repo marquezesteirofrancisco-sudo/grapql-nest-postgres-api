3f96d23c718227c7c6c87b0872d6ad03043b4cc7
Francisco Marquez Esteiro <117361707+franciscomes@users.noreply.github.com>
2026-04-15 19:56:45 +0200

    Buscar Usuario FindOne
    Buscar Usuario FindOne

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,13 +1,15 @@
+import { ParseUUIDPipe, UseGuards } from '@nestjs/common';
+
 import { Resolver, Query, Mutation, Args, Int, ID } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
 import { ValidRolesArgs } from './dto/args/roles.arg';
-import { UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guards';
 import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 import { ValidRoles } from 'src/auth/enums/valid-roles.enum';
+import { parse } from 'path';
 
 @Resolver(() => User)
 @UseGuards(JwtAuthGuard)
@@ -25,9 +27,22 @@ export class UsersResolver {
     return this.usersService.findAll(validRoles.roles);
   }
 
-  @Query(() => User, { name: 'user' })
-  findOneByEmail(@Args('email', { type: () => String }) email: string) : Promise<User> {
+  @Query(() => User, { name: 'userByEmail' })
+  findOneByEmail(
+    @Args('email', { type: () => String }) email: string,
+    @CurrentUser( [ValidRoles.admin, ValidRoles.superUser]) user: User
+
+  ) : Promise<User> {
     return this.usersService.findOneByEmail(email);
+  }
+
+  @Query(() => User, { name: 'userById' })
+  findOneById(
+    @Args('id', { type: () => ID }, ParseUUIDPipe) id: string,
+    @CurrentUser( [ValidRoles.admin, ValidRoles.superUser]) user: User
+
+  ) : Promise<User> {
+    return this.usersService.findOneById(id);
   }
 
 /*   @Mutation(() => User)
