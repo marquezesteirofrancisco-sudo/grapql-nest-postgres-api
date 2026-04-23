d237fead90ba8e967e6d2c35b68fdb811069aeed
Francisco Marquez Esteiro <117361707+franciscomes@users.noreply.github.com>
2026-04-23 17:35:30 +0200

    Seed: Borrar registros
    Seed: Borrar registros

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Item } from 'src/items/entities/item.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class SeedService {
 
     private isProd: boolean = false;
 
-constructor(private readonly configService: ConfigService) 
+constructor(private readonly configService: ConfigService,
+    @InjectRepository(Item) 
+    private readonly itemsRepository: Repository<Item>,
+
+    @InjectRepository(User) 
+    private readonly usersRepository: Repository<User>
+) 
 {
     this.isProd = this.configService.get('STATE') === 'prod';
+    
 }
 
 async executeSeed(): Promise<boolean> {
@@ -18,11 +29,26 @@ async executeSeed(): Promise<boolean> {
         }
 
         // Limpiar la base de datos, BORRAR DATOS
+        await this.deleteDatabase();
 
         // Crear Usuarios
 
         // Crear Items
 
         return true;
+    }
+
+    async deleteDatabase() {
+
+        await this.itemsRepository.createQueryBuilder()
+            .delete()
+            .where({})
+            .execute();
+
+
+        await this.usersRepository.createQueryBuilder()
+            .delete()
+            .where({})
+            .execute();
     }
 }
